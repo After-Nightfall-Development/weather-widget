2035345a46337665db2690d71a5f828b0b4e5e34
joe-afternightfall <joe@afternightfall.com>
2021-02-04 08:00:18 -0600

    adding jest/lint and tsconfig

---
+++ jest.config.js
@@ -0,0 +1,37 @@
+module.exports = {
+    clearMocks: true,
+    collectCoverage: true,
+    collectCoverageFrom: [
+        "**/*.ts",
+        "**/*.tsx"
+    ],
+    coverageThreshold: {
+        global: {
+            branches: 85,
+            functions: 90,
+            lines: 90,
+            statements: 90
+        }
+    },
+    coverageDirectory: "coverage",
+    coveragePathIgnorePatterns: [
+        "/coverage/",
+        "/src/configs",
+        "/src/App.tsx",
+        "/src/index.tsx",
+        "/node_modules/",
+        "/src/react-app-env.d.ts"
+    ],
+    moduleFileExtensions: ["ts", "tsx", "js", "json", "jsx"],
+    rootDir: ".",
+    testEnvironment: "jsdom",
+    testMatch: [
+        "**/*.test.tsx",
+        "**/*.test.ts"
+    ],
+    transform: {
+        "\\.ts$": "ts-jest",
+        "\\.tsx$": "ts-jest"
+    },
+    verbose: false,
+};
