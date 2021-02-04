4864a48c6bd2933c021471c2a3408499a5cba088
joe-afternightfall <joe@afternightfall.com>
2021-02-04 07:59:58 -0600

    setup

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,7 @@
+import App from './App';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import reportWebVitals from './report-web-vitals';
 
 ReactDOM.render(
   <React.StrictMode>
